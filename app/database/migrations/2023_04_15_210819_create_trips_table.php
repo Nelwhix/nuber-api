c64baaaaa1a827643e029da0822604cd3425d49a
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-04-15 22:21:09 +0100

    added migrations

---
+++ app/database/migrations/2023_04_15_210819_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->ulid();
+            $table->foreignUlid('user_id');
+            $table->foreignUlid('driver_id');
+            $table->boolean('is_started')->default(false);
+            $table->boolean('is_complete')->default(false);
+            $table->json('origin')->nullable();
+            $table->json('destination')->nullable();
+            $table->string('destination_name')->nullable();
+            $table->json('driver_location')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
